ff54fc1506abc5083aa8d98750cea6999b47940c
rcr0008 <rcr0008@alu.medac.es>
2022-06-07 12:22:27 +0200

    Creado comanda nueva y añadir cola metodo

--- src/rodrirecu/CarrilComanda.java
+++ src/rodrirecu/CarrilComanda.java
@@ -10,5 +10,23 @@ package rodrirecu;
  * @author Usuario
  */
 public class CarrilComanda {
+ Comanda carril[] = new Comanda [10];
+    int ncomandas;
+    Comanda servidas [] = new Comanda[10];
+    int servida = 0;
+    int adelantada =0;
 
+    public CarrilComanda(int n) {
+        this.ncomandas = n;
+        for (int i = 0; i < this.ncomandas; i++) {
+        int beber = (int) Math.round((Math.random() * 4) + 1);
+            this.carril[i] = new Comanda(beber,"Hamburguesa",1);
+        }
+    }
+
+    public void añadirComanda() {
+        this.ncomandas++;
+        int beber = (int) Math.round((Math.random() * 4) + 1)
+        this.carril[i] = new Comanda(beber,"Hamburguesa",1);
+    }
 }
--- src/rodrirecu/Comanda.java
+++ src/rodrirecu/Comanda.java
@@ -10,5 +10,21 @@ package rodrirecu;
  * @author Usuario
  */
 public class Comanda {
+int bebida; //1.Coca cola//2.Nestea//3.Fanta Limon//4.Fanta Naranja//
+ String comida;
+ int numcomanda;
+    public Comanda(int b, String comida, int numcomanda){
+        this.bebida = b ;
+        this.comida = e;
+        this.numcomanda = numcomanda;
+    
 
+    
+    public String toString() {
+        return "Comanda " + "Comida: " + this.comida +" Bebida: "+ this.bebida + "\n" ;
+    }
+    
+    
 }
+}
+
