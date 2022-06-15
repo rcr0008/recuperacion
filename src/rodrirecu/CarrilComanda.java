65b2bf5783429b5eff38e37b6208c4559aaae200
Guillermo Callejas Rodriguez <rcr0008@alu.medac.es>
2022-06-15 13:39:46 +0200

    Dos nuevos metodos añadidos adelantar y retrasar

--- src/rodrirecu/CarrilComanda.java
+++ src/rodrirecu/CarrilComanda.java
@@ -9,23 +9,47 @@ package rodrirecu;
  * @author Usuario
  */
 public class CarrilComanda {
-    Comanda carril[] = new Comanda [10];
+    Comanda carril[] = new Comanda[10];
     int ncomandas;
-    Comanda servidas [] = new Comanda[10];
+    Comanda servidas[] = new Comanda[10];
     int servida = 0;
-    int adelantada =0;
+    int adelantada = 0;
 
     public CarrilComanda(int n) {
         this.ncomandas = n;
         for (int i = 0; i < this.ncomandas; i++) {
-        int beber = (int) Math.round((Math.random() * 4) + 1);
-            this.carril[i] = new Comanda(beber,"Hamburguesa",1);
+            int beber = (int) Math.round((Math.random() * 4) + 1);
+            this.carril[i] = new Comanda(beber, "Hamburguesa", 1);
         }
     }
 
     public void anadirComanda() {
         this.ncomandas++;
         int beber = (int) Math.round((Math.random() * 4) + 1);
-        this.carril[ncomandas] = new Comanda(beber,"Hamburguesa",1);
+        this.carril[ncomandas] = new Comanda(beber, "Hamburguesa", 1);
+    }
+    
+    
+     public void adelantar() {
+        this.carril[0]=null;
+        this.adelantada++;
+        for (int i = 1; i < ncomandas; i++) {
+            if(this.adelantada>this.ncomandas){
+                System.out.println("No se puede adelantar comandas");
+            }else{
+                this.carril[i] = this.carril[i - 1];
+            }
+            
+        }
+    }
+     public void retrasar() {
+        for (int i = 1; i < ncomandas; i++) {
+            if (this.ncomandas == this.carril.length) {
+                System.out.println("El carril de comandas se encuentra completo");
+            } else {
+                this.carril[i] = this.carril[i + 1];
+            }
+        }
+        ncomandas++;
     }
 }
